de39c61bfc79968f3fc8351cbc920bca656e1f7f
Michael Christenson <michaelchristenson@Michaels-MacBook-Pro.local>
2017-08-25 16:53:17 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/queen-of-hearts.png"
+	},	
+];
+
 const cardsInPlay = [];
 
 const checkForMatch = () => {
@@ -13,8 +35,10 @@ const checkForMatch = () => {
 }
 
 const flipCard = cardId => {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 
