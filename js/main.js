cc0e35f936fa37f4c6740bda2182b012af0f883d
Michael Christenson <michaelchristenson@Michaels-MacBook-Pro.local>
2017-08-25 00:08:38 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+console.log("user flipped " + cardOne);
+cardsInPlay.push(cardTwo);
+console.log("user flipper " + cardTwo);
 
-const cardOne = "queen";
-const cardTwo = "king";
-const cardThree = "queen";
-const cardFour = "king";
-
-console.log("User flipped " + cardFour);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
